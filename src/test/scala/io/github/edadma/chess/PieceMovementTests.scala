28754038405a9c7e69476a4a2b9c84d2f8d8c018
Edward A Maxedon <edadma@gmail.com>
2025-01-18 21:02:03 -0500

    add BoardStateTests

---
+++ src/test/scala/io/github/edadma/chess/PieceMovementTests.scala
@@ -0,0 +1,6 @@
+package io.github.edadma.chess
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+class PieceMovementTests extends AnyFreeSpec with Matchers {}
