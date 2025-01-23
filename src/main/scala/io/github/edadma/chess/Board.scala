5fff93d98c8f7c1b8f3ffe81af8eceef3270d08e
Edward A Maxedon Sr <edadma@gmail.com>
2025-01-23 04:33:06 -0500

    basic board

--- src/main/scala/io/github/edadma/chess/Board.scala
+++ src/main/scala/io/github/edadma/chess/Board.scala
@@ -1,5 +1,7 @@
 package io.github.edadma.chess
 
+import scala.language.postfixOps
+
 val A1 = "A1"; val B1 = "B1"; val C1 = "C1"; val D1 = "D1"; val E1 = "E1"; val F1 = "F1"; val G1 = "G1"; val H1 = "H1"
 val A2 = "A2"; val B2 = "B2"; val C2 = "C2"; val D2 = "D2"; val E2 = "E2"; val F2 = "F2"; val G2 = "G2"; val H2 = "H2"
 val A3 = "A3"; val B3 = "B3"; val C3 = "C3"; val D3 = "D3"; val E3 = "E3"; val F3 = "F3"; val G3 = "G3"; val H3 = "H3"
@@ -9,7 +11,7 @@ val A6 = "A6"; val B6 = "B6"; val C6 = "C6"; val D6 = "D6"; val E6 = "E6"; val F
 val A7 = "A7"; val B7 = "B7"; val C7 = "C7"; val D7 = "D7"; val E7 = "E7"; val F7 = "F7"; val G7 = "G7"; val H7 = "H7"
 val A8 = "A8"; val B8 = "B8"; val C8 = "C8"; val D8 = "D8"; val E8 = "E8"; val F8 = "F8"; val G8 = "G8"; val H8 = "H8"
 
-def parseBoardString(board: String): Set[(Int, Int, Piece)] = {
+def parseBoardString(board: String): Set[(Int, Piece)] = {
   val lines = board.split("\n").map(_.trim).filter(_.nonEmpty)
   val pieces =
     for {
@@ -18,8 +20,7 @@ def parseBoardString(board: String): Set[(Int, Int, Piece)] = {
       (piece, x) <- chars.zipWithIndex
       if piece != '.'
     } yield (
-      x,
-      7 - y,
+      (7 - y) * 8 + x,
       piece match {
         case 'P' => WhitePawn
         case 'N' => WhiteKnight
@@ -51,7 +52,26 @@ trait ChessBoard {
   def isSquareAttacked(square: Int, by: Side): Boolean
 }
 
-case class Board(pieces: Map[String, Piece], lastMove: Option[Move]) extends ChessBoard:
+object Board {
+  def apply: Board =
+    Board.fromString(
+      """
+       r  n  b  q  k  b  n  r
+       p  p  p  p  p  p  p  p
+       .  .  .  .  .  .  .  .
+       .  .  .  .  .  .  .  .
+       .  .  .  .  .  .  .  .
+       .  .  .  .  .  .  .  .
+       P  P  P  P  P  P  P  P
+       R  N  B  Q  K  B  N  R
+     """,
+    )
+
+  def fromString(board: String): Board =
+    Board(parseBoardString(board) map ((square, piece) => toAlgebraic(square) -> piece) toMap)
+}
+
+case class Board(pieces: Map[String, Piece], lastMove: Option[Move] = None) extends ChessBoard:
   def getPiece(square: Int): Option[Piece] = pieces.get(toAlgebraic(square))
   def applyMove(move: Move): ChessBoard =
     val from  = toAlgebraic(move.fromIndex)
@@ -59,6 +79,18 @@ case class Board(pieces: Map[String, Piece], lastMove: Option[Move]) extends Che
     val to    = toAlgebraic(move.toIndex)
 
     Board(pieces + (to -> piece) - from, Some(move))
+
+  def isLegalMove(move: Move): Boolean = ???
+
+  def getLegalMoves: Iterator[Move] = ???
+
+  def isInCheck(side: Side): Boolean = ???
+
+  def isCheckmate(side: Side): Boolean = ???
+
+  def isStalemate(side: Side): Boolean = ???
+
+  def isSquareAttacked(square: Int, by: Side): Boolean = ???
 
 enum PieceType {
   case KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN
