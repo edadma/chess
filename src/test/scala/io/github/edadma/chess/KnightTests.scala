74d30a373a88b478f8b7e4b35ec02f9bb5fe1e42
Edward A Maxedon <edadma@gmail.com>
2025-01-25 19:36:33 -0500

    kights

--- src/test/scala/io/github/edadma/chess/KnightTests.scala
+++ src/test/scala/io/github/edadma/chess/KnightTests.scala
@@ -79,4 +79,32 @@ class KnightTests extends ChessSpec {
       }
     }
   }
+
+  "side" - {
+    val board = Board.fromString(
+      """
+        |.  .  .  .  .  .  .  .
+        |.  .  .  .  .  .  .  .
+        |.  .  .  .  .  .  .  n
+        |.  .  .  .  .  .  .  .
+        |.  .  .  .  .  .  .  .
+        |.  .  .  .  .  .  .  .
+        |.  .  .  .  .  .  .  .
+        |.  .  .  .  .  .  .  .
+        |""".stripMargin,
+    )
+
+    val expectedAttacks = List(
+      (6, 3),
+      (5, 4),
+      (5, 6),
+      (6, 7),
+    ).map { case (f, r) => r * 8 + f }
+
+    expectedAttacks.foreach { square =>
+      s"square ${toAlgebraic(square)} should be attacked" in {
+        assert(board.isSquareAttacked(square, Black))
+      }
+    }
+  }
 }
