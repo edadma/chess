efc04963f98b208518ce4322ef51185bd4c435b2
Edward A Maxedon Sr <edadma@gmail.com>
2025-01-28 21:57:06 -0500

    SAN

--- src/main/scala/io/github/edadma/chess/Game.scala
+++ src/main/scala/io/github/edadma/chess/Game.scala
@@ -81,6 +81,80 @@ class Game(start: ChessBoard = Board()) {
     else if (isInsufficientMaterial) Draw(DrawReason.InsufficientMaterial)
     else Ongoing(currentTurn)
   }
+
+  def lastMoveToSAN: String = {
+    moves.headOption.map(move => moveToSAN(move, moves.tail.headOption)).getOrElse("")
+  }
+
+  private def moveToSAN(move: ChessMove, previousMove: Option[ChessMove]): String = {
+    val builder = new StringBuilder
+    val previousBoard = previousMove match {
+      case Some(prevMove) => moves.tail.foldLeft(start)((b, m) => if (m != prevMove) b.applyMove(m) else b)
+      case None           => if (moves.length > 1) moves.tail.foldLeft(start)((b, m) => b.applyMove(m)) else start
+    }
+
+    move.moveType match {
+      case MoveType.CASTLE_KINGSIDE  => builder.append("O-O")
+      case MoveType.CASTLE_QUEENSIDE => builder.append("O-O-O")
+      case _ =>
+        if (move.piece.pieceType != PieceType.PAWN) {
+          builder.append(pieceToChar(move.piece))
+          // Disambiguation
+          val otherPieces = previousBoard.getPiecesBySide(move.piece.side).filter { case (square, piece) =>
+            piece.pieceType == move.piece.pieceType && square != move.fromIndex &&
+            previousBoard.getMoves(move.piece.side).exists(m => m.fromIndex == square && m.toIndex == move.toIndex)
+          }
+          if (otherPieces.nonEmpty) {
+            val fromFile = move.fromIndex % 8
+            val fromRank = move.fromIndex / 8
+            if (otherPieces.exists(_._1 % 8 == fromFile)) {
+              builder.append((fromRank + '1').toChar)
+            } else {
+              builder.append((fromFile + 'a').toChar)
+            }
+          }
+        }
+
+        // Handle captures
+        val isCapture = previousBoard.getPiece(move.toIndex).isDefined ||
+          (move.piece.pieceType == PieceType.PAWN && (move.fromIndex % 8) != (move.toIndex % 8))
+        if (isCapture) {
+          if (move.piece.pieceType == PieceType.PAWN) {
+            builder.append((move.fromIndex % 8 + 'a').toChar)
+          }
+          builder.append('x')
+        }
+
+        // Destination square
+        builder.append((move.toIndex % 8 + 'a').toChar)
+        builder.append((move.toIndex / 8 + 1).toChar)
+
+        // Pawn promotion
+        move.promotion.foreach(p => {
+          builder.append('=')
+          builder.append(pieceToChar(p))
+        })
+    }
+
+    // Check or checkmate
+    val afterBoard = previousBoard.applyMove(move)
+    if (afterBoard.isCheckmate(move.piece.side.opposite)) {
+      builder.append('#')
+    } else if (afterBoard.isInCheck(move.piece.side.opposite)) {
+      builder.append('+')
+    }
+
+    builder.toString
+  }
+
+  private def pieceToChar(piece: Piece): Char = piece.pieceType match {
+    case PieceType.KING   => 'K'
+    case PieceType.QUEEN  => 'Q'
+    case PieceType.ROOK   => 'R'
+    case PieceType.BISHOP => 'B'
+    case PieceType.KNIGHT => 'N'
+    case PieceType.PAWN   => ' '
+  }
 }
 
 sealed trait GameStatus
